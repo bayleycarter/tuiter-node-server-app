a0286322805923265591bb73503c29a9ed648490
Bayley Carter <carter.ba@northeastern.edu>
2022-11-30 22:29:13 -0500

    a9

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ import cors from 'cors';
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
+// mongoose.connect('mongodb+srv://bcarter1:<password>@cluster0.t22rzg9.mongodb.net/?retryWrites=true&w=majority');
 
 const app = express()
 app.use(cors())
